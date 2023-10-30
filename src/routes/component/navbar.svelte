e5403aa581a738bb4342ced3ebad275820071da0
watchakorn-18k <porton2559@gmail.com>
2023-10-31 01:59:10 +0700

    Update for build in gitpage

--- src/routes/component/navbar.svelte
+++ src/routes/component/navbar.svelte
@@ -4,7 +4,7 @@
 
 <div class="navbar bg-base-300">
   <div class="flex-1">
-    <a href="/" class="btn btn-ghost normal-case text-xl h-16">
+    <a href="{base}/" class="btn btn-ghost normal-case text-xl h-16">
       <div class="flex flex-col">
         <p>Ppython Basic</p>
         <p class="text-[12px]">เพราะพื้นฐานสำคัญที่สุด</p>
