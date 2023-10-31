576d30f88be899e2958416959a5a173647660916
watchakorn-18k <porton2559@gmail.com>
2023-10-31 23:03:52 +0700

    Update website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,16 @@
         alt="logo"
         class="w-32 rounded-full"
       />
-      <h1 class="text-5xl font-bold">Ppython Basic</h1>
-      <p class="py-6">
+      <h1 class="text-2xl md:text-5xl font-bold">Ppython Basic</h1>
+      <iframe
+        class="w-full h-64 lg:w-[40rem] lg:h-[22rem] rounded-lg"
+        width="100%"
+        src="https://www.youtube.com/embed/XHzJAOPaFrs"
+        frameborder="0"
+        allowfullscreen
+        title="Ppython Basic"
+      />
+      <p class="py-">
         สอนพื้นฐานการเขียนโปรแกรมด้วย Python เบื้องต้น
         หรือจะให้ช่วยติวข้อสอบก็ได้หรือใครสนใจจะทำเว็บไซต์หรือโปรแกรม
         ขึ้นเป็นของตัวเอง หรือเขียนบอท Discord , Line , Telegram
@@ -24,11 +32,26 @@
       <button class="btn btn-primary" on:click={openModal}
         >รายเอียดที่สอน</button
       >
-      <button
-        class="btn btn-success"
-        on:click={() => window.open("https://lin.ee/CbNYGle")}
-        >แอดไลน์ : @621jnpbb</button
-      >
+      <div class="flex gap-4 flex-wrap justify-center">
+        <button
+          class="btn bg-[#06c755] text-white"
+          on:click={() => window.open("https://lin.ee/CbNYGle")}
+          ><img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/LINE_logo.svg/768px-LINE_logo.svg.png"
+            class="w-8 shadow-md rounded-xl"
+            alt=""
+          />: @621jnpbb</button
+        >
+        <button
+          class="btn bg-[#000001] text-white"
+          on:click={() => window.open("https://twitter.com/ppybasic_python")}
+          ><img
+            src="https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png"
+            class="w-6 shadow-md rounded-xl"
+            alt=""
+          />: @ppybasic_python</button
+        >
+      </div>
     </div>
   </div>
 </div>
