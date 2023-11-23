ef4600ab380438044b8d114d1a1ec678d2279198
watchakorn-18k <porton2559@gmail.com>
2023-11-23 22:23:35 +0700

    fix: code build

--- svelte.config.js
+++ svelte.config.js
@@ -22,8 +22,12 @@ const config = {
       // use relative URLs similar to an anchor tag <a href="/test/1"></a>
       // do not include group layout folders in the path such as /(group)/test/1
       entries: [
+        "/articles/article-content",
         "/articles/article-content/commit-github",
         "/articles/article-content/format-string-python",
+        "/course/web-react-basic",
+        "/course/web-python-basic",
+        "/course/python-basic",
       ],
     },
     paths: {
