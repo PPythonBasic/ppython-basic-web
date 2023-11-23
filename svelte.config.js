cc1f1a2a149a344d0f8dc67640d1c1f6abd9f9fa
watchakorn-18k <porton2559@gmail.com>
2023-11-23 22:20:56 +0700

    fix: code build

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,14 @@ const config = {
       precompress: false,
       strict: true,
     }),
+    prerender: {
+      // use relative URLs similar to an anchor tag <a href="/test/1"></a>
+      // do not include group layout folders in the path such as /(group)/test/1
+      entries: [
+        "/articles/article-content/commit-github",
+        "/articles/article-content/format-string-python",
+      ],
+    },
     paths: {
       base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
     },
